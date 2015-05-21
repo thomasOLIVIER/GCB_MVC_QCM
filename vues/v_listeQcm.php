07ea29a7339218e2d55b7689070a01acdfd5c2f9
Thomas <thomas.olivier30@hotmail.fr>
2015-05-21 15:40:39 +0200

    Parties opérationnelles

--- vues/v_listeQcm.php
+++ vues/v_listeQcm.php
@@ -1,32 +1,4 @@
 ﻿<div id="contenu">
-<table class="listeLegere">
-  	   <caption>Administration des QCM
-       </caption>
-             <tr>
-                <th class="libelle">Libellé</th>   
-                <th class="action">Supprimer</th>
-                <th class="action">Modifier</th> 
-             </tr>
-          
-    <?php    
-	    foreach( $lesQcm as $unQcm) 
-		{
-			$libelle = $unQcm['libelle'];
-                        $id = $unQcm['identifiant'];
-	?>		
-            <tr>
-                <td><?php echo $libelle?></td>
-                <td><a href="index.php?uc=gererQcm&action=supprimerQcm&identifiant=<?php echo $id ?>" 
-				onclick="return confirm('Voulez-vous vraiment supprimer ce QCM?');"><img src=".\images\icones\b_drop.png"/></a></td>
-                <td><a href="index.php?uc=gererQcm&action=validerMajQcm&identifiant=<?php echo $id ?>" 
-				onclick="return confirm('Voulez-vous vraiment modifier ce QCM?');"><img src=".\images\icones\b_edit.png"/></a></td>
-             </tr>
-	<?php		 
-          
-          }
-	?>
-</table>
-
     <form id="frmCreationFrais" action="index.php?uc=gererQcm&action=validerCreationQcm" method="post">
         <div class="corpsForm">
             <fieldset>
@@ -47,6 +19,34 @@
             </p> 
         </div>
     </form>
+<table class="listeLegere">
+  	   <caption>Administration des QCM
+       </caption>
+             <tr>
+                <th class="libelle">Libellé</th>   
+                <th class="action">Supprimer</th>
+                <th class="action">Modifier</th> 
+             </tr>
+          
+    <?php    
+	    foreach( $lesQcm as $unQcm) 
+		{
+			$libelle = $unQcm['libelle'];
+                        $id = $unQcm['identifiant'];
+	?>		
+            <tr>
+                <td><?php echo $libelle?></td>
+                <td><a href="index.php?uc=gererQcm&action=supprimerQcm&identifiant=<?php echo $id ?>" 
+				onclick="return confirm('Voulez-vous vraiment supprimer ce QCM?');"><img src=".\images\icones\b_drop1.png"/></a></td>
+                <td><a href="index.php?uc=gererQcm&action=validerMajQcm&identifiant=<?php echo $id ?>" 
+				onclick="return confirm('Voulez-vous vraiment modifier ce QCM?');"><img src=".\images\icones\b_edit1.png"/></a></td>
+             </tr>
+	<?php		 
+          
+          }
+	?>
+</table>
+
 </div>	
 
 
